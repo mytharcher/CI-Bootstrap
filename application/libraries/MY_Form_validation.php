b7bd8b38ce02f8db66077236bea2e6de1b28e370
mytharcher <mytharcher@gmail.com>
2013-12-01 02:08:23 +0800

    Enhancement of validation rule 'is_unique' for ignoring update operation on self record.

--- application/libraries/MY_Form_validation.php
+++ application/libraries/MY_Form_validation.php
@@ -6,4 +6,27 @@ class MY_Form_validation extends CI_Form_validation {
 	{
 		return $this->_error_array;
 	}
+
+	/**
+	 * Match one field to another
+	 *
+	 * @access	public
+	 * @param	string
+	 * @param	field
+	 * @return	bool
+	 */
+	public function is_unique($str, $field)
+	{
+		// var_dump(123);
+		list($table, $field)=explode('.', $field);
+		list($field, $key)=explode('!', $field);
+		$this->CI->db->limit(1)->where($field, $str);
+		$value = $this->CI->uri->segment(4);
+		if ($key && $value !== FALSE) {
+			$this->CI->db->where("$key <>", $value);
+		}
+		$query = $this->CI->db->get($table);
+		
+		return $query->num_rows() === 0;
+    }
 }
