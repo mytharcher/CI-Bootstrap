dcfc5c6c28dd7ed10a60642912309dc79897bc1c
mytharcher <mytharcher@gmail.com>
2015-03-17 23:25:35 +0800

    Update env sample files.

--- env/domain.php
+++
@@ -1,7 +0,0 @@
-<?php  if ( ! defined('BASEPATH')) exit('No direct script access allowed');
-
-$config['app_domain'] = 'localhost';
-
-if (!isset($_SERVER['HTTP_HOST'])) {
-	$_SERVER['HTTP_HOST'] = $config['app_domain'];
-}
---
+++ env/sample/smarty.php
@@ -0,0 +1,7 @@
+<?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+
+// Smarty caching enabled by default unless explicitly set to FALSE
+$config['cache_status'] = FALSE;
+
+// Debug mode turned on or off (TRUE / FALSE)
+$config['smarty_debug'] = FALSE;
